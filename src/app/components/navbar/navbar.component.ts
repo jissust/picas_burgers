5f8a50ac12b570711a2ec7d5b3ac9fb60292016b
jesustissera <jesustissera@gmail.com>
2024-06-10 20:52:52 -0300

    funcionamiento de navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  public option: string = 'home';
+  stateScroll  = false;
 
+  constructor(private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  selectOption(option: string){
+    this.option = option;
+  }
+
+  @HostListener('document:scroll')
+  
+  scrollFunction(){
+    if(document.documentElement.scrollTop > 10){
+      this.stateScroll = true
+    }else{
+      this.stateScroll = false
+    }
+  }
 }
